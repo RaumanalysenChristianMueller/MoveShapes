3b26e73068205313f80066e49215b0e6f5363e71
RaumanalysenChristianMueller <raumanalysen@mailbox.org>
2018-02-05 23:31:54 +0100

    Further improvements considering video codec settings in order to increase video quality

--- moveShapes.R
+++ moveShapes.R
@@ -61,92 +61,92 @@ if (pics){
 t <- 1
 
 # set number of frames for transitions
-ntrans <- 30
+ntrans <- 90
 
 # calculate number of frames
 nt <- length(shps_names) * ntrans
 
-# set animation options
-ani.options(
-  nmax = 1000000000000000,
-  interval = 0.1,
-  ani.width = 1920,
-  ani.height = 1440,
-  ani.dev = 'jpeg',
-  ani.type = 'jpeg'
-  )
-
 
 # create progress bar
-pr_bar <- winProgressBar(title = "moveShapes - Raumanalysen_Christian_Müller", label = "moveShapes arbeitet",
+pr_bar <- winProgressBar(title = "moveShapes - Raumanalysen_Christian_Müller", label = "moveShapes erzeugt einzelne Bilder",
                          min = 1, max = nt, width = 500)
 
-# create animation
-saveVideo({
-
-  # create plots
-  for (s in 1:length(shps_names)){
+# create individual frames as png
+# create plots
+for (s in 1:length(shps_names)){
     
-    # define starting and end state of current transition 
-    this_1 <- get(paste0("shp_", s))
-    if (s < length(shps_names)) this_2 <- get(paste0("shp_", s + 1)) else this_2 <- this_1
+  # define starting and end state of current transition 
+  this_1 <- get(paste0("shp_", s))
+  if (s < length(shps_names)) this_2 <- get(paste0("shp_", s + 1)) else this_2 <- this_1
     
-    # iterate over each transition
-    for (f in 1:ntrans){
+  # iterate over each transition
+  for (f in 1:ntrans){
+  
+    # save additionally as png
+    png(filename = paste0(im_dir, "/animationFrame_", t, ".png"), width = 1920, height = 1440)
+      
+    # set plot options
+    layout(matrix(c(1,2), 2, 1), heights = c(1,7))
+      
+    # update time bar
+    par(mar = c(1, 2, 0, 2))
+    plot(1:nt, rep(1, nt), axes = F, type = "n", xlab = "", ylab = "")
+    axis(1, at = seq(1, nt, length.out = length(shps_names)),
+         labels = shps_show_names, las = 1, cex.axis = 2)
+    segments(x0 = t, x1 = t, y0 = 0, y1 = 1, col = "coral")
+      
+    # plot backgroud
+    par(mar = c(5, 3, 3, 3))
+    plot(bg, col = "lightblue", border = "lightblue")
+      
+    # plot shape 1 with decreasing opacity
+    col1 <- rgb(255, 127, 80, seq(0, 255, length.out = ntrans)[ntrans - f], maxColorValue = 255)
+    plot(this_1, col = col1, border = col1, add = T)
+      
+    # plot shape 1 with increasing opacity
+    col2 <- rgb(255, 127, 80, seq(0, 255, length.out = ntrans)[f], maxColorValue = 255)
+    plot(this_2, col = col2, border = col2, add = T)
     
-      # save additionally as png
-      if (pics){
-        png(filename = paste0(im_dir, "/animationFrame_", t, ".png"),
-            width = 1920, height = 1440)
-      }
+    # add legend
+    legend("bottomright", cex = 2,
+           fill = c("lightblue", "coral"),
+           legend = c(bg_shp_name_show, lg_name_show))
       
-      # set plot options
-      layout(matrix(c(1,2), 2, 1),
-             heights = c(1,7))
+    # close image file
+    dev.off()
       
-      # update time bar
-      par(mar = c(1, 2, 0, 2))
-      plot(1:nt, rep(1, nt), axes = F, type = "n", xlab = "", ylab = "")
-      axis(1, at = seq(1, nt, length.out = length(shps_names)),
-           labels = shps_show_names, las = 1, cex.axis = 2)
-      segments(x0 = t, x1 = t, y0 = 0, y1 = 1, col = "coral")
-      
-      # plot backgroud
-      par(mar = c(5, 3, 3, 3))
-      plot(bg, col = "lightblue", border = "lightblue")
-      
-      # plot shape 1 with decreasing opacity
-      col1 <- rgb(255, 127, 80, seq(0, 255, length.out = ntrans)[ntrans - f], maxColorValue = 255)
-      plot(this_1, col = col1, border = col1, add = T)
-      
-      # plot shape 1 with increasing opacity
-      col2 <- rgb(255, 127, 80, seq(0, 255, length.out = ntrans)[f], maxColorValue = 255)
-      plot(this_2, col = col2, border = col2, add = T)
     
-      # add legend
-      legend("bottomright", cex = 2,
-             fill = c("lightblue", "coral"),
-             legend = c(bg_shp_name_show, lg_name_show))
+    # increase count variable and progress bar
+    setWinProgressBar(pr_bar, t,
+                      label = paste0("Einen Moment Geduld...", round(t / nt * 100, 0), " %"))
+    t <- t + 1
       
-      # close image file
-      if (pics) dev.off()
-      
-      # load generated image to plot device
-      png_im <- readPNG(paste0(im_dir, "/animationFrame_", t, ".png"))
-      plot.new()
-      rasterImage(png_im, par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4])
-      
-      # increase count variable and progress bar
-      t <- t + 1
-      setWinProgressBar(pr_bar, t,
-                        label = paste0("Einen Moment Geduld...", round(t / nt * 100, 0), " %"))
       
     }
     
-  }
+}
   
-},
-  video.name = paste0(save_dir, "/", video_name),
-  other.opts = '-b 400k -r 30 -fs 1000000000000000')
+# close progress bar
+close(pr_bar)
 
+# create new progress bar
+pr_bar <- winProgressBar(title = "moveShapes - Raumanalysen_Christian_Müller", label = "moveShapes fügt einzelne Bilder zu einem Video zusammen",
+                         min = 1, max = 100, width = 500)
+setWinProgressBar(pr_bar, 80,
+                  label = paste0("Einen Moment Geduld...80 %"))
+
+# building a system command line
+sys_com <- paste0(
+  "ffmpeg ",
+  "-y -framerate 30 ",
+  "-i ",
+  im_dir, "/animationFrame_%d.png ",
+  "-s:v 1920x1440 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ",
+  save_dir, "/", video_name
+)
+
+# creating a video from individual png files
+system(sys_com)
+
+# close progress bar
 close(pr_bar)
